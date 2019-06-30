46dd77f910c9ee2432f0991bae6d951cd2257fcd
Jiahao XU <Jiahao_XU@outlook.com>
2019-07-01 00:54:30 +1000

    Add function Exit_if_failed and Eval_and_Exit_if_failed in func_collection.sh to replace all 'if ; then' block used in func_collection.sh and install-and-configure-docker.sh.

--- func_collection.sh
+++ func_collection.sh
@@ -1,15 +1,34 @@
 verify_docker_install() {
     echo -e "\nVerifing docker install"
+
     docker run --rm hello-world
     exit_status=$?
+
     docker rmi hello-world
-    if [ $exit_status -ne 0 ]; then
-        echo -e "\nDocker is not installed properly, something went wrong"
-        exit 1
-    fi
+
+    Exit_if_failed $exit_status "\nDocker is not installed properly, something went wrong"
+
+    #if [ $exit_status -ne 0 ]; then
+    #    echo -e "\nDocker is not installed properly, something went wrong"
+    #    exit 1
+    #fi
 }
 
 restart_docker() {
     echo -e "\nRestarting docker"
     /etc/init.d/docker restart
+}
+
+# If [ $1 -ne 0 ], print $2 and exit.
+Exit_if_failed() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2"
+        exit 1
+    fi
+}
+
+# eval "$1". If it failed, print $2 and exit.
+Eval_and_Exit_if_failed() {
+    eval "$1"
+    Exit_if_failed $? "$2"
 }
--- install-and-configure-docker.sh
+++ install-and-configure-docker.sh
@@ -2,29 +2,35 @@
 
 source ./func_collection.sh
 
-if [ $UID -ne 0 ]; then
-    echo "Does not have enough permission, please run as root"
-    exit 1
-fi
+Exit_if_failed $UID "Does not have enough permission, please run as root"
+#if [ $UID -ne 0 ]; then
+#    echo "Does not have enough permission, please run as root"
+#    exit 1
+#fi
 
 # Install docker-ce using distro's software manager
 if grep -q -e "Debian" -e "debian" /etc/*-release; then
     echo -e "\nInstall necessary softwares for adding docker-ce apt source with gpg key"
-    apt update && \
-    apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+    apt update && apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
     echo -e "\nInstalling fingerprint for docker-ce source"
     curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
     echo -e "\nVerifing fingerprint of the key"
     apt-key fingerprint 0EBFCD88 2>/dev/null | grep -q "9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88"
-    if [ $? -eq 1 ]; then
-        echo "Failed to verify the fingerprint"
-        exit 1
-    fi
+    Eval_and_Exit_if_failed "[ $? -ne 1 ]" "Failed to verify the fingerprint"
+    #if [ $? -eq 1 ]; then
+    #    echo "Failed to verify the fingerprint"
+    #    exit 1
+    #fi
 
     echo -e "\nAdding docker-ce apt source"
     add-apt-repository "deb https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+    Exit_if_failed $? "Failed to add the docker debian source"
+    #if [ $? -ne 0 ]; then
+    #    echo "Failed to add the docker debian source"
+    #    exit 1
+    #fi
 
     echo -e "\nInstalling docker-ce"
     apt update
@@ -40,10 +46,14 @@ verify_docker_install
 
 echo -e "\nStart to configure docker\nEnable usernamespace first"
 sysctl >/dev/null 2>&1
-if [ $? -eq 127 ]; then
-    echo -e "\nCommand 'sysctl' not found and thus cannot enable user namespace"
-    exit 1
-fi
+
+Eval_and_Exit_if_failed "[ $? -ne 127 ]" "\nCommand 'sysctl' not found and thus cannot enable user namespace"
+
+#if [ $? -eq 127 ]; then
+#    echo -e "\nCommand 'sysctl' not found and thus cannot enable user namespace"
+#    exit 1
+#fi
+
 sysctl -w kernel.unprivileged_userns_clone=1
 
 echo -e "\nEnabling user remap feature in docker using user namespace"
@@ -53,15 +63,19 @@ restart_docker
 
 echo -e "Verifing the user remap feature"
 id dockremap
-if [ $? -ne 0 ]; then
-    echo -e "\nDockerd failed to create dockremap user"
-    exit 1
-fi
+Exit_if_failed $? "\nDockerd failed to create dockremap user"
+
+#if [ $? -ne 0 ]; then
+#    echo -e "\nDockerd failed to create dockremap user"
+#    exit 1
+#fi
 
 grep dockremap /etc/subuid && grep dockremap /etc/subgid
-if [ $? -ne 0 ]; then
-    echo -e "\nDockerd failed to setup user remap feature properly"
-    exit 1
-fi
+Exit_if_failed $? "\nDockerd failed to setup user remap feature properly"
+
+#if [ $? -ne 0 ]; then
+#    echo -e "\nDockerd failed to setup user remap feature properly"
+#    exit 1
+#fi
 
 verify_docker_install
