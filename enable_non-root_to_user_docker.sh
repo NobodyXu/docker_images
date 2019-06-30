a8603f670b1d472fc845839e4a8b3ef602b028d3
Jiahao XU <Jiahao_XU@outlook.com>
2019-07-01 01:03:09 +1000

    Revert "Use Exit_if_failed and Eval_and_Exit_if_failed to prevent repeated 'if ; then' block of code"
    This reverts commit 91e6a7b0c22ebefada2e2735cdb66154e5a7ee71.

--- enable_non-root_to_user_docker.sh
+++ enable_non-root_to_user_docker.sh
@@ -3,17 +3,15 @@
 source ./func_collection.sh
 
 user="$1"
-Eval_and_Exit_if_failed "[ -n '$user' ]" "Usage: ./enable_non-root_user_of_docker.sh username"
-#if [ -z "$user" ]; then
-#    echo -e "Usage: ./enable_non-root_user_of_docker.sh username"
-#    exit 1
-#fi
+if [ -z "$user" ]; then
+    echo -e "Usage: ./enable_non-root_user_of_docker.sh username"
+    exit 1
+fi
 
-Exit_if_failed $UID "Must run as root!"
-#if [ $UID -ne 0 ]; then
-#    echo "Must run as root!"
-#    exit 1
-#fi
+if [ $UID -ne 0 ]; then
+    echo "Must run as root!"
+    exit 1
+fi
 
 groupadd docker
 usermod -aG docker $user
