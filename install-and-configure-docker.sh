2124cb087f42474cd33acd48d72e0adfbb020362
Jiahao XU <Jiahao_XU@outlook.com>
2019-06-30 23:34:46 +1000

    Fix the problem with checking docker debian source key that even if the right key is installed, due to the way 'apt-key' output the hash of the finger (it has an additional space in the middle), the test is considered 'failed' while it is not.

--- install-and-configure-docker.sh
+++ install-and-configure-docker.sh
@@ -17,7 +17,7 @@ if grep -q -e "Debian" -e "debian" /etc/*-release; then
     curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
     echo -e "\nVerifing fingerprint of the key"
-    apt-key fingerprint 0EBFCD88 | grep -q "9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88"
+    apt-key fingerprint 0EBFCD88 | grep -q "9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88"
     if [ $? -eq 1 ]; then
         echo "Failed to verify the fingerprint"
         exit 1
