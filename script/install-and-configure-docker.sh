518db036d672e0f01f2ad865922a6416294d813b
Jiahao XU <Jiahao_XU@outlook.com>
2019-07-02 22:36:42 +1000

    Add support for ubuntu and raspbian (including a workaround for buster)

--- script/install-and-configure-docker.sh
+++ script/install-and-configure-docker.sh
@@ -7,6 +7,13 @@ script_dir="$(cd $(dirname ${BASH_SOURCE[0]}) >/dev/null 2>&1 && pwd)"
 
 source "${script_dir}/func_collection.sh"
 
+is_accessible() {
+    # option -f tells curl to fail silently with no output
+    # option -I tells curl to only download the header
+    # option -s puts curl in quiet mode, with no progress meter or error message
+    curl -fIs "${1}" --output /dev/null
+}
+
 Exit_if_failed $UID "Does not have enough permission, please run as root"
 #if [ $UID -ne 0 ]; then
 #    echo "Does not have enough permission, please run as root"
@@ -16,40 +23,59 @@ Exit_if_failed $UID "Does not have enough permission, please run as root"
 # Get system distro and version
 source <(sed -e "s/^/SYSTEM_/g" /etc/os-release)
 
+failed=false
 # Install docker-ce using distro's software manager
-if [ $SYSTEM_ID_LIKE = "debian" ] && [ $SYSTEM_ID != "ubuntu" ]; then
-    progress_log "Uninstall old, incompatible version of docker-ce"
-    apt remove docker docker-engine docker.io containerd runc
+if [ $SYSTEM_ID_LIKE = "debian" ]; then
+    base_url="https://download.docker.com/linux/${SYSTEM_ID}"
+    if ! is_accessible "${base_url}/dists/${SYSTEM_VERSION_CODENAME}/stable"; then
+	# Try using the distro it is derived from
+	# Only works for raspbian
+        base_url="https://download.docker.com/linux/${SYSTEM_ID_LIKE}"
+        if ! is_accessible "${base_url}/dists/${SYSTEM_VERSION_CODENAME}/stable"; then
+            failed=true
+        fi
+    fi
 
-    progress_log "Install necessary softwares for adding docker-ce apt source with gpg key"
-    apt update && apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-    Exit_if_failed $? "\nFailed to install the necessary softwares for adding docker-ce apt source with gpg key"
+    if ! $failed; then
+        progress_log "Uninstall old, incompatible version of docker-ce"
+        apt remove -y docker docker-engine docker.io containerd runc
 
-    progress_log "Installing fingerprint for docker-ce source"
-    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+        progress_log "Install necessary softwares for adding docker-ce apt source with gpg key"
+        apt update && apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+        Exit_if_failed $? "\nFailed to install the necessary softwares for adding docker-ce apt source with gpg key"
 
-    progress_log "Verifing fingerprint of the key"
-    apt-key fingerprint 0EBFCD88 2>/dev/null | grep -q "9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88"
-    Eval_and_Exit_if_failed "[ $? -ne 1 ]" "Failed to verify the fingerprint"
-    #if [ $? -eq 1 ]; then
-    #    echo "Failed to verify the fingerprint"
-    #    exit 1
-    #fi
+        progress_log "Installing fingerprint for docker-ce source"
+        curl -fsSL "${base_url}/gpg" | apt-key add -
 
-    progress_log "Adding docker-ce apt source"
-    add-apt-repository "deb https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-    Exit_if_failed $? "Failed to add the docker debian source"
-    #if [ $? -ne 0 ]; then
-    #    echo "Failed to add the docker debian source"
-    #    exit 1
-    #fi
+        progress_log "Verifing fingerprint of the key"
+        apt-key fingerprint 0EBFCD88 2>/dev/null | grep -q "9DC8 5822 9FC7 DD38 854A  E2D8 8D81 803C 0EBF CD88"
+        Eval_and_Exit_if_failed "[ $? -ne 1 ]" "Failed to verify the fingerprint"
+        #if [ $? -eq 1 ]; then
+        #    echo "Failed to verify the fingerprint"
+        #    exit 1
+        #fi
 
-    progress_log "Installing docker-ce"
-    apt update && apt install docker-ce docker-ce-cli containerd.io
-    Exit_if_failed $? "Failed to install docker-ce"
+        progress_log "Adding docker-ce apt source -- will overwrite /etc/apt/sources.list.d/docker-ce.list"
+	repository="${base_url} ${SYSTEM_VERSION_CODENAME} stable"
+	echo -e "deb ${repository}\ndeb-src ${repository}">/etc/apt/sources.list.d/docker-ce.list
+        Exit_if_failed $? "Failed to add the docker debian source"
+        #if [ $? -ne 0 ]; then
+        #    echo "Failed to add the docker debian source"
+        #    exit 1
+        #fi
 
-    progress_log "Install completed"
-else
+	progress_log "Installing docker-ce (first removing it)"
+
+        list="docker-ce docker-ce-cli containerd.io"
+        apt remove -y $list && apt update && apt install -y $list
+
+        Exit_if_failed $? "Failed to install docker-ce"
+
+        progress_log "Install completed"
+    fi
+fi
+
+if $failed; then
     progress_log "Fallback to convenience script provided by docker"
     curl -fsSL https://get.docker.com | sh
     Exit_if_failed $? "Failed to install docker-ce by using convenience script provided by docker"
