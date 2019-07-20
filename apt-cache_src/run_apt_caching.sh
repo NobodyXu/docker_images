ef819469727f035d621d24e64fa74a65e2d96240
Jiahao XU <Jiahao_XU@outlook.com>
2019-07-20 15:00:22 +1000

    Add image git-cache for caching 'git clone', also reusing apt-cahce_src/run_apt_caching.sh by generalizing it

--- apt-cache_src/run_apt_caching.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if ! docker ps -a | grep -q "apt-caching"; then
-    docker run -d --restart on-failure --name apt-caching apt-cache
-elif ! docker ps | grep -q "apt-caching"; then
-    docker start apt-caching
-fi
---
+++ run_cache.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: run_cache.sh (will-be)container_name image_name"
+fi
+
+if ! docker ps -a | grep -q "$1"; then
+    docker run -d --restart on-failure --name $1 $2
+elif ! docker ps | grep -q "$1"; then
+    docker start $1
+fi
