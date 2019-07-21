2574958f1998412eb0820053acd2c45b0df7d298
Jiahao XU <Jiahao_XU@outlook.com>
2019-07-21 22:36:19 +1000

    Add feature in run_cache.sh: If the container already exists, remove it and start a new one in case that the image is updated.

--- run_cache.sh
+++ run_cache.sh
@@ -5,7 +5,14 @@ if [ -z "$1" ] || [ -z "$2" ]; then
     exit 1
 fi
 
+# If the container already exists, remove it and start a new one
+# in case that the image is updated.
 if ! docker ps -a | grep -q "$1"; then
-    docker run -d --restart on-failure --name $1 $2
-elif ! docker ps | grep -q "$1"; then
+    # Stop the container first if it is running.
+    if ! docker ps | grep -q "$1"; then
+        docker stop "$1"
+    fi
+    docker rm "$1"
 fi
+
+exec docker run -d --restart on-failure --name "$1" "$2"
