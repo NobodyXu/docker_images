b14a1148f655aef3f68f023c5acf08d0e0fcfbbe
Jiahao XU <Jiahao_XU@outlook.com>
2019-07-22 23:22:44 +1000

    Add run_install_clang.sh: Define wrapper for command 'git' to prevent git from failing in low-speed area

---
+++ build-env-base_src/run_install_clang.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# A simple git wrapper to prevent git from failing in low-speed area
+# due to timeout or whatever.
+git() {
+    while true; do
+        /usr/bin/env git "$@"
+        if [ $? -ne 0 ] && [ -n "$1" ] && [ "$1" = "clone" ]; then
+            continue
+        fi
+	break
+    done
+}
+
+source /tmp/install-clang/install-clang -j $(nproc) -C /opt/clang
